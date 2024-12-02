2bc6e30e8403d6875a0fc78d6a981e439e112f89
Ilyas <ilyas.ahmad.mscs-s2022a@cecosian.edu.pk>
2024-12-02 11:14:18 +0500

    update

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+import Medium from '../pageAction/Medium'
+
+
+
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://medium.com/')
+
+
+    it('[TC01] User Should be able to visit Medium Website ', () => {
+      Medium.MediumCICD()
+    })
+
+  })
+})
---
+++ cypress/pageAction/Medium.js
@@ -0,0 +1,19 @@
+
+const SignIn = '//a[text() = "Sign in"]'
+const CreateOne = '//b[text() = "Create one"]'
+
+class Medium {
+
+
+
+    MediumCICD() {
+
+
+        cy.xpath(SignIn).click()
+        cy.wait(1000)
+        cy.xpath(CreateOne).click()
+        cy.wait(2000)
+
+    }
+}
+export default new Medium
